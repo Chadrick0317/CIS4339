8f25374ea3f0ddadf646cecc5b13cea145b46f59
Chadrick0317 <70494157+Chadrick0317@users.noreply.github.com>
2021-10-09 16:50:35 -0400

    Add files via upload

---
+++ CIS4339_Exam1_cjohns31/models/attendees.js
@@ -0,0 +1,23 @@
+// const uuid = require('uuid');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let gradeSchema = new Schema({
+    courseNumber: {
+      type: String
+    },
+    grade: {
+      type: Number
+    },
+    studentID: {
+      type: Number,
+      required: true
+    }
+  }, {
+    collection: 'grades'
+});
+
+module.exports = mongoose.model('grade', gradeSchema)
+
+//versionKey: false 
+
---
+++ CIS4339_Exam1_cjohns31/models/event.js
@@ -0,0 +1,33 @@
+const uuid = require('uuid');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let studentSchema = new Schema({
+    //_id: Number,
+    _id: { type: String, default: uuid.v1 },
+    firstName: {
+      type: String
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+      type: String
+    },
+    phoneNumber: {
+      type: Number
+    },
+    studentID: {
+      type: Number,
+      required: true
+    }
+  }, {
+    collection: 'students'
+});
+
+module.exports = mongoose.model('student', studentSchema)
+
+//versionKey: false 
+
+
+
